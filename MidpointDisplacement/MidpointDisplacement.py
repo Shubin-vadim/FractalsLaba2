a7816e76d9d20fe9058eb13830be52d4410909fe
Vadim Shubin <liliaa23ar@gmail.com>
2023-05-19 19:11:25 +0300

    first version DiamondDisplacement algorithm

---
+++ MidpointDisplacement/MidpointDisplacement.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import random
+
+
+def midpoint_displacement(xL, yL, xR, yR, roughness, threshold):
+    if abs(xR - xL) < threshold:
+        return [(xL, yL), (xR, yR)]
+
+    xM = (xL + xR) / 2
+    yM = (yL + yR) / 2 + (random.random() * 2 - 1) * roughness * abs(xR - xL)
+
+    left_segment = midpoint_displacement(xL, yL, xM, yM, roughness, threshold)
+    right_segment = midpoint_displacement(xM, yM, xR, yR, roughness, threshold)
+
+    return left_segment + right_segment[1:]
+
+
+def generate_terrain(width, height, roughness, threshold):
+    global terrain
+    terrain = [(0, height // 2), (width, height // 2)]
+
+    terrain = midpoint_displacement(0, height // 2, width, height // 2, roughness, threshold)
+
+    return terrain
+
+
+def plot_terrain(terrain):
+    x = [point[0] for point in terrain]
+    y = [point[1] for point in terrain]
+
+    plt.fill_between(x, y, min(y), color='black')
+    plt.fill_between(x, y, max(y), color='blue')
+    plt.plot(x, y, color='green')
+
+    plt.axis('off')
+    plt.show()
+
+
+def update_terrain():
+    terrain = generate_terrain(width, height, roughness, threshold)
+    plot_terrain(terrain)
+
+
+# Настройки
+width = 800  # Ширина изображения
+height = 400  # Высота изображения
+threshold = 2 # Пороговое значение длины отрезка
+scroll_offset = 200  # Смещение для скроллинга
+
+#xl, yl, xr, yr = map(float, input().split())
+roughness = 5 #float(input())
+terrain = generate_terrain(width, height, roughness, threshold)
+plot_terrain(terrain)
--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.25)
+
+t = np.arange(0.0, 1.0, 0.001)
+
+a0 = 6
+f0 = 3
+s = a0 * np.sin(2 * np.pi * f0 * t)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+pl, = plt.plot(t, s, lw=3, color='green')
+plt.axis([0, 1, -10, 10])
+axcolor = 'White'
+
+frequency_axis = plt.axes(
+    [0.25, 0.1, 0.65, 0.03],
+    facecolor=axcolor
+)
+
+amplitude_axis = plt.axes(
+    [0.25, 0.15, 0.65, 0.03],
+    facecolor=axcolor
+)
+
+frequency_slider = Slider(frequency_axis, 'Freq', 0.1, 30.0, valinit=f0)
+amplitude_slider = Slider(amplitude_axis, 'Amp', 0.1, 10.0, valinit=a0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def update(val):
+    amp = amplitude_slider.val
+    freq = frequency_slider.val
+    pl.set_ydata(amp * np.sin(2 * np.pi * freq * t))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+frequency_slider.on_changed(update)
+amplitude_slider.on_changed(update)
+
+plt.show()

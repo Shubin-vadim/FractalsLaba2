41e989d85ea9ca7bf4f15d74661bc6eea4a7e08f
Vadim Shubin <midavnibush@gmail.com>
2023-05-22 11:04:22 +0300

    add presentation

--- DiamondSquare/DiamondSquare.py
+++ DiamondSquare/DiamondSquare.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import ndarray
 
 
-def diamond_square(n, R, initial_heights) -> None:
-    size = 2**n + 1
+def diamond_square(n, R, initial_heights) -> ndarray:
+    size = 2 ** n + 1
     height_map = np.zeros((size, size))
 
     # Установка начальных высот для углов квадрата
@@ -12,27 +13,27 @@ def diamond_square(n, R, initial_heights) -> None:
     height_map[size-1, 0] = initial_heights[2]
     height_map[size-1, size-1] = initial_heights[3]
 
-    step = size - 1
+    step = size
     while step > 1:
         half_step = step // 2
 
-        # Square step
-        for i in range(half_step, size-1, step):
-            for j in range(half_step, size-1, step):
+        # Square
+        for i in range(half_step, size - 1, step):
+            for j in range(half_step, size - 1, step):
                 # Среднее арифметическое вершин сдвинутое на случайную величину
-                height_map[i, j] = (height_map[i-half_step, j-half_step] +
-                                    height_map[i-half_step, j+half_step] +
-                                    height_map[i+half_step, j-half_step] +
-                                    height_map[i+half_step, j+half_step]) / 4 + np.random.uniform(-R * half_step, R * half_step)
+                height_map[i, j] = (height_map[i - half_step, j - half_step] +
+                                    height_map[i - half_step, j + half_step] +
+                                    height_map[i + half_step, j - half_step] +
+                                    height_map[i + half_step, j + half_step]) / 4 + (np.random.random() * 2 - 1) * half_step * R
 
-        # Diamond step
-        for i in range(0, size-1, half_step):
-            for j in range((i + half_step) % step, size-1, step):
+        # Diamond
+        for i in range(0, size - 1, half_step):
+            for j in range((i + half_step) % step, size - 1, step):
                 # Среднее арифметическое вершин сдвинутое на случайную величину
-                height_map[i, j] = (height_map[(i-half_step) % size, j] +
-                                    height_map[(i+half_step) % size, j] +
-                                    height_map[i, (j-half_step) % size] +
-                                    height_map[i, (j+half_step) % size]) / 4 + np.random.uniform(-R * half_step, R * half_step)
+                height_map[i, j] = (height_map[(i - half_step) % size, j] +
+                                    height_map[(i + half_step) % size, j] +
+                                    height_map[i, (j - half_step) % size] +
+                                    height_map[i, (j + half_step) % size]) / 4 + (np.random.random() * 2 - 1) * half_step * R
 
         step = half_step
 
@@ -45,10 +46,10 @@ R = float(input("Введите значение параметра R: "))  # Ш
 
 # Ввод высот углов
 print("Введите высоты углов:")
-top_left = float(input("Верхний левый угол: "))
-top_right = float(input("Верхний правый угол: "))
-bottom_left = float(input("Нижний левый угол: "))
-bottom_right = float(input("Нижний правый угол: "))
+top_left = int(input("Верхний левый угол: "))
+top_right = int(input("Верхний правый угол: "))
+bottom_left = int(input("Нижний левый угол: "))
+bottom_right = int(input("Нижний правый угол: "))
 initial_heights = [top_left, top_right, bottom_left, bottom_right]
 
 # Генерация карты высот
--- MidpointDisplacement/MidpointDisplacement.py
+++ MidpointDisplacement/MidpointDisplacement.py
@@ -13,7 +13,6 @@ def midpoint_displacement(xL, yL, xR, yR, roughness, threshold):
     xM = (xL + xR) / 2
     yM = (yL + yR) / 2 + (random.random() * 2 - 1) * roughness * abs(xR - xL)
 
-    roughness/=2
     left_segment = midpoint_displacement(xL, yL, xM, yM, roughness, threshold)
     right_segment = midpoint_displacement(xM, yM, xR, yR, roughness, threshold)
 
@@ -21,18 +20,17 @@ def midpoint_displacement(xL, yL, xR, yR, roughness, threshold):
 
 
 def generate_terrain(width, height, roughness, threshold):
-    global terrain
-
     terrain = midpoint_displacement(0, height // 2, width, height // 2, roughness, threshold)
-
     return terrain
 
 
-def plot_terrain(terrain):
+def plot(terrain, C, f=None):
     x = [point[0] for point in terrain]
-    y = [point[1]  for point in terrain]
-    # print(x)
-    # print(max(y))
+    if f is not None:
+        y = [f(point[1]) + C for point in terrain]
+    else:
+        y = [point[1] + C for point in terrain]
+
     plt.fill_between(x, y, min(y) - 10, color='black')
     plt.fill_between(x, y, max(y) + 10, color='blue')
     plt.plot(x, y, color='green')
@@ -44,10 +42,14 @@ def plot_terrain(terrain):
 # Настройки
 width = 500  # Ширина изображения
 height = 500  # Высота изображения
-threshold = 1  # Пороговое значение длины отрезка
-C = 0.5
+threshold = int(input('Введите пороговое значение длины отрезка (целое число) '))
+roughness = float(input('Введите параметр шероховатости '))
+C = float(input('Введите константу для построцессинга (0 - без построцессинга) '))
+is_f = input('Желаете ли вы использовать функцию f(x) = x^2 в качестве построцессинга высот? 1 - Да, 0 - нет. ')
 
+points = generate_terrain(width, height, roughness, threshold)
 
-R = 400
-terrain = generate_terrain(width, height, R, threshold)
-plot_terrain(terrain)
+if is_f == '1':
+    plot(points, C, f)
+else:
+    plot(points, C)
